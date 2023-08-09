0dedd06cc56f8800643d897f22ffe320b658b56f
elboqueronpaco <elboqueronpaco@gmail.com>
2023-08-09 14:48:58 +0200

    configuracion del cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'https://digitalpaco.vercel.app/',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
